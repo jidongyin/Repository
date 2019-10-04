220f111de062bbb88342605b3cb47c75123f3832
jidongyin <Administrator@ZP4A46I8KKWX5BQ>
2019-10-04 09:29:19 +0800

    4

--- spirngcloud-fanout-consumer/src/main/java/com/bjsxt/PushReceiver.java
+++ spirngcloud-fanout-consumer/src/main/java/com/bjsxt/PushReceiver.java
@@ -47,6 +47,7 @@ public class PushReceiver {
 		System.out.println("11111111111111111111111111111111111111");
 		
 		System.out.println("22222222222222222222222222222");
+		System.out.println("44444444444444444");
 
 	}
 
